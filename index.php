65966662aad4c624cb72bc8c61b00676e03476ec
Simka94 <simakalexandr94@gmail.com>
2017-04-01 14:03:27 +0300

    Portfolio

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Portfolio v2.0</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
+        <script src="js/js.js"></script>
+    </head>
+    <body>   
+        <div class="preloader">
+            <div class="loared_container">
+                <div class="loader"></div>
+                <div class="loader"></div>
+                <div class="loader"></div>
+            </div>
+        </div>
+        <div class="main_container">
+        <div class="navigation_container">
+            <div class="navigation">
+                <div class="logo">logo</div>
+                <div class="nav_menu">menu</div>
+                <div class="footer">footer</div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="content_container">
+                Hello
+            </div>
+        </div> 
+        </div>
+    </body>
+</html>
---
+++ js/js.js
@@ -0,0 +1,5 @@
+$(document).ready(function(i){
+    setTimeout(function(){
+        $('.preloader').addClass('close_preloader');
+    },2000);
+}); 
