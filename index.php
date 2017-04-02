ba1c7a92fe127bca0a2c7583e1301e8c39ad7b41
Simka94 <simakalexandr94@gmail.com>
2017-04-02 12:18:21 +0300

    port

--- index.php
+++ index.php
@@ -1,32 +1,138 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Portfolio v2.0</title>
         <link rel="stylesheet" href="css/style.css">
         <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
         <script src="js/js.js"></script>
     </head>
     <body>   
+<!--
         <div class="preloader">
             <div class="loared_container">
                 <div class="loader"></div>
                 <div class="loader"></div>
                 <div class="loader"></div>
-            </div>
+            </div> 
         </div>
+-->
         <div class="main_container">
-        <div class="navigation_container">
-            <div class="navigation">
-                <div class="logo">logo</div>
-                <div class="nav_menu">menu</div>
-                <div class="footer">footer</div>
+            <div class="navigation_container">
+                <div class="navigation">
+                    <div class="logo">logo</div>
+                    <div class="nav_menu">
+                        <ul class="nav">
+                            <div id="animator"></div>
+                            <li class="active_anim">
+                                <a class="nav_link" href="#main">Главная</a>
+                            </li>
+                            <li>
+                                <a class="nav_link" href="#about">Обо мне</a>
+                            </li>
+                            <li>
+                                <a class="nav_link" href="#skills">Скилы</a>
+                            </li>
+                            <li>
+                                <a class="nav_link" href="#portfolio">Портфолио</a>
+                            </li>
+                            <li>
+                                <a class="nav_link" href="#contacts">Контакты</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="footer">footer</div>
+                </div>
             </div>
-        </div>
-        <div class="section">
-            <div class="content_container">
-                Hello
-            </div>
-        </div> 
+            <div class="section">
+                <div id="main" class="main section_container">
+                    Hello
+                </div>
+                <div id="about" class="about section_container">
+                    Hello
+                </div>
+                <div id="skills" class="skills section_container">
+                    Hello
+                </div>
+                <div id="portfolio" class="portfolio section_container">
+                    <div class="work fbs">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="https://fbs-tax.com/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work didius">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://www.didiusdesign.com/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work school">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://smartschool.in.ua/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work rollo">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://rollowolf.com/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work sayhi">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://media-talks.say-hi.me/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work ubernet">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://ubernet.com.ua/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work vienterir">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://vinterior.com.ua/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work rp">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://rozoomni-podaroonky.com.ua/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work sib">
+                        <div class="animation_name">
+                            <div class="work_name">FBS</div>
+                            <div class="link">
+                                <a href="http://sib.community/" target="_blank">Посмотреть</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="contacts" class="contacts section_container">
+                    Hello
+                </div>
+            </div> 
         </div>
     </body>
 </html>
--- js/js.js
+++ js/js.js
@@ -1,5 +1,50 @@
-$(document).ready(function(i){
+$(document).ready(function(){
     setTimeout(function(){
         $('.preloader').addClass('close_preloader');
     },2000);
+    
+    
+    var $root = $('body');
+    $('.nav_link').click(function() {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+    
+    
+    var sections = $('.section_container'),
+      nav = $('.nav li');
+
+    $(window).on('scroll', function () {
+      var cur_pos = $(this).scrollTop();
+
+      sections.each(function() {
+        var top = $(this).offset().top - ($(this).outerHeight() / 2),
+            bottom = top + $(this).outerHeight();
+            
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').parent().removeClass('active_anim');
+            sections.removeClass('active_anim');
+
+            $(this).addClass('active_anim');
+            nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active_anim');
+            if($(this).attr('id') == 'main'){
+                $('#animator').css({'top':'14px'});
+            }else if($(this).attr('id') == 'about'){
+                $('#animator').css({'top':'60px'});
+            }else if($(this).attr('id') == 'skills'){
+                $('#animator').css({'top':'105px'});
+            }else if($(this).attr('id') == 'portfolio'){
+                $('#animator').css({'top':'150px'});
+            }else if($(this).attr('id') == 'contacts'){
+                $('#animator').css({'top':'195px'});
+            }
+        }
+      });
+    });
+    
+    
+    
 }); 
