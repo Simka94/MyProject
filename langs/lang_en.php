b667cf143b1c40063baa59e004c3b28ec9d6ddf3
Simka94 <simakalexandr94@gmail.com>
2017-05-04 15:12:01 +0300

    ROOT

--- langs/lang_en.php
+++ langs/lang_en.php
@@ -5,8 +5,8 @@ define('job','Front-end Developer');
 define('description','Front-end Developer producing high quality responsive websites.');
 define('about_me_text','
 My name is Alexander, I\'m 23 years old. I graduated from NTUU "KPI", majoring in microprocessor technology. For more than two years I have been professionally engaged in creating websites. I have a lot of experience in creating adaptive, cross-browser sites. I am able to work in a team.');
-define('about_skills_text','HTML, CSS / SASS / LESS, JS / jQuery / AJAX, PHP, MySQL. Worked with Wordpres, Opencard, Joomla. With graphic editors Photoshop, Illustrator. I am able to work with vector graphics. I check the validity of the code through the W3C validator, speed through Google Speed. I use the Gulp project collector and Git version control system.');
-define('about_desires_text','In the near future, I plan to develop in the direction of the backend. Find decent work and enjoy the solution of various tasks.');
+define('about_skills_text','HTML, CSS / SASS / LESS, JS / jQuery / AJAX, PHP, MySQL. Worked with WordPress, OpenCart, Joomla. With graphic editors Photoshop, Illustrator. I am able to work with vector graphics. I check the validity of the code through the W3C validator, speed through Google Speed. I use the Gulp project collector and Git version control system.');
+define('about_desires_text','In the near future, I plan to develop in the direction of the backend. Enjoy the solution of various tasks.');
 
 define('about_me','About me');
 define('about_skills','About skills');
